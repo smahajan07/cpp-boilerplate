4c540313d1d2981e5a3bffb4067eeb0729e240f3
smahajan07 <mahajan.sarthak28@gmail.com>
2018-09-22 00:00:23 -0400

    created basic class structure for pid controller

---
+++ include/pid.h
@@ -0,0 +1,18 @@
+#ifndef PID_H_
+#define PID_H_
+
+#include<iostream>
+
+class pidController(){
+	private:
+		float k_p;
+		float k_i;
+		float k_d;
+	public:
+		float setpointVal;
+		float initVel;
+		float computeVelocity();
+};
+
+#endif
+
