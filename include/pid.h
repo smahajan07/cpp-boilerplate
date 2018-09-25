d706607ac04cb1475aa0dbe118c10abf39d73f54
smahajan07 <mahajan.sarthak28@gmail.com>
2018-09-24 23:32:59 -0400

    update header file for part 1 (compute method undefined)

--- include/pid.h
+++ include/pid.h
@@ -1,17 +1,26 @@
-#ifndef PID_H_
-#define PID_H_
+/*
+ * Copyright (c) 2018, Sarthak Mahajan
+ * All rights reserved.
+ *
+ * Author: sarthak
+ */
 
-#include<iostream>
+#ifndef INCLUDE_PID_H_
+#define INCLUDE_PID_H_
+
+#include <iostream>
 
 class pidController{
   private:
     float k_p;
     float k_i;
     float k_d;
+    double dt;
   public:
-    float setpointVal;
-    float initVel;
-    float computeVelocity();
+    pidController();
+    double integral;
+    double prevError;
+    double computeVelocity(double& , double& );
 };
 
 #endif
