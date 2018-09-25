418636068b85740e90bcc30ca1271a7ad262bd26
smahajan07 <mahajan.sarthak28@gmail.com>
2018-09-25 00:16:29 -0400

    adding unit tests

--- test/pid_test.cpp
+++ test/pid_test.cpp
@@ -1,5 +1,40 @@
-#include <gtest/gtest.h>
+/*
+ * Copyright (c) 2018, Sarthak Mahajan
+ * All rights reserved.
+ *
+ * Author: sarthak
+ */
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+/** @file pid_test.cpp
+ * @brief To test a PID controller
+ *
+ */
+
+#include <gtest/gtest.h>
+#include "pid.h"
+
+/**
+ * @brief Unit test to check time interval dt
+ *
+ * test to ensure that the value of dt is not zero
+ * to avoid division-by-zero error
+ */
+
+TEST(timeInterval, timeIntervalNotZero) {
+  pidController obj1;
+  EXPECT_NE(0, obj1.dt);
+}
+
+/**
+ * @brief Unit test for compute method
+ *
+ * Test to check if compute function returns the
+ * same velocity as desired velocity if their is
+ * no error
+ */
+
+TEST(computeTest, noError) {
+  pidController obj2;
+  double vI = 10,vF = 10;
+  EXPECT_EQ(vF, obj2.computeVelocity(vI, vF));
 }
